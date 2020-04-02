387e0b07fd031f0a08a2efe5706de8cdbe625e95
Macen Vargas <38738974+mvargas9517@users.noreply.github.com>
2020-04-02 01:23:05 -0500

    Added like onpress

--- lib/feed_page.dart
+++ lib/feed_page.dart
@@ -4,9 +4,13 @@ import 'package:google_fonts/google_fonts.dart';
 import 'models/post.dart';
 
 class FeedPage extends StatefulWidget {
+
+  
   @override
   _FeedPageState createState() => _FeedPageState();
 }
+
+Color _iconwhite = Colors.white;
 
 class Story {
   String name;
@@ -17,6 +21,8 @@ class Story {
     this.image = image;
   }
 }
+
+
 
 class _FeedPageState extends State<FeedPage> {
 
@@ -45,8 +51,6 @@ class _FeedPageState extends State<FeedPage> {
        Story('Kelsey383', 'images/image6.jpg'),
      );
    }
-
-   
 
 
   @override
@@ -145,11 +149,11 @@ class _FeedPageState extends State<FeedPage> {
                 width: _width,
                 child: ListView.builder(
                   scrollDirection: Axis.vertical,
-                  itemCount: posts.length,
+                  itemCount: post.length,
                    padding: EdgeInsets.only(bottom: 10),
                   itemBuilder: (context, index) {
                     return Container(
-                          height: _height,
+                          height: _height * 0.91,
                           width: _width,
                           color: Colors.black,
                           child: Padding(
@@ -167,41 +171,48 @@ class _FeedPageState extends State<FeedPage> {
                                                         backgroundColor: Colors.red,
                                                           child: CircleAvatar(
                                                           radius: 12,
-                                                          backgroundImage: AssetImage('images/image$index.jpg'), 
+                                                          backgroundImage: AssetImage('images2/image2$index.jpg'), 
                                                         ),
                                                       ),
                                                       SizedBox(width: 10.0,),
-                                                      Text(posts[index].name,
+                                                      Text(post[index].name,
                                                       style: TextStyle(
                                                         color: Colors.white,
                                                         fontWeight: FontWeight.w500
                                                       ),),
                                               ],
                                             ),
-                                            SizedBox(height: 10.0,),
+                                            SizedBox(height: 12.0,),
                                             Container(
                                               height: _height * 0.55,
                                               child: Image(image: AssetImage('images/post$index.jpg'), fit: BoxFit.cover),
                                             ),
-                                            SizedBox(height: 10.0,),
                                             Row(
                                           children: <Widget>[
+                                            SizedBox(width: 0.0,),
+                                                IconButton(
+                                                  icon: Icon(Icons.favorite_border),
+                                                  color: _iconwhite,
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      _iconwhite = Colors.red;
+                                                    });
+                                                  },
+                                              ),
+                                            SizedBox(width: 0.0,),
+                                            Icon(MaterialCommunityIcons.chat_outline, color: Colors.white, size: 25.0),
                                             SizedBox(width: 10.0,),
-                                            Icon(Feather.heart, color: Colors.white),
-                                            SizedBox(width: 10.0,),
-                                            Icon(MaterialCommunityIcons.chat_outline, color: Colors.white),
-                                            SizedBox(width: 10.0,),
-                                            Icon(Feather.send, color: Colors.white),
+                                            Icon(Feather.send, color: Colors.white, size: 25.0),
                                           ],
-                                        ),
-                                        SizedBox(height: 12.0,),
+                                            ),
+                                        SizedBox(height: 3.0,),
                                          Column(
                                                 crossAxisAlignment: CrossAxisAlignment.start,
                                                 children: <Widget>[
                                                   Row(
                                                     children: <Widget>[
                                                       SizedBox(width: 10.0,),
-                                                      Text(posts[index].likes,
+                                                      Text(post[index].likes,
                                                           style: TextStyle(
                                                             color: Colors.white,
                                                             fontSize: 18.0,
@@ -216,44 +227,73 @@ class _FeedPageState extends State<FeedPage> {
                                                           ),), 
                                                     ],
                                                   ),
-                                                  SizedBox(height: 10.0),
+                                                  SizedBox(height: 12.0),
                                                       Row(
                                                         crossAxisAlignment: CrossAxisAlignment.start,
                                                           children: <Widget>[
                                                             SizedBox(width: 10.0,),
-                                                              Text(posts[index].name,
+                                                              Text(post[index].name,
                                                                   style: TextStyle(
                                                                     color: Colors.white,
-                                                                    fontSize: 18.0,
+                                                                    fontSize: 16.0,
                                                                     fontWeight: FontWeight.bold
                                                                   ),),
                                                                 SizedBox(width: 10.0,),
-                                                                Text(posts[index].desc,
+                                                                Text(post[index].desc,
                                                                 style: TextStyle(
                                                                   fontSize: 16.0,
                                                                   color: Colors.white,
                                                                 ),),
                                                           ],
                                                         ),
-                                                        SizedBox(height: 5.0,),
+                                                        SizedBox(height: 8.0,),
                                                       Padding(
                                                         padding: const EdgeInsets.only(left: 10.0),
                                                         child: Text('View all 234 comments',
-                                                        style: TextStyle(
-                                                          color: Colors.grey,
-                                                          fontSize: 18.0,
-                                                        ),),
+                                                            style: TextStyle(
+                                                              color: Colors.grey,
+                                                              fontSize: 18.0,
+                                                            ),),
+                                                      ), 
+                                                      SizedBox(height: 8.0,),
+                                                    Padding(
+                                                      padding: const EdgeInsets.only(left: 10.0),
+                                                      child: Column(
+                                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                                        children: <Widget>[
+                                                          Row(
+                                                            children: <Widget>[
+                                                              Text(post[index].user,
+                                                          style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontWeight: FontWeight.bold,
+                                                          ),),
+                                                          SizedBox(width: 5.0),
+                                                              Text(post[index].comment,
+                                                              style: TextStyle(
+                                                                color: Colors.white,
+                                                                fontSize: 16.0
+                                                              )),
+                                                            ],
+                                                          ),
+                                                          SizedBox(height: 8.0,),
+                                                          Text(post[index].time,
+                                                              style: TextStyle(
+                                                                color: Colors.grey,
+                                                                fontSize: 16.0
+                                                              )),
+                                                        ],
                                                       ),
-                                                      
+                                                    ),   
                                                 ],
                                               ),
                                           ],
                                         ),
                                     Positioned(
                                       right: 20.0,
-                                      bottom: 252.0,
+                                      bottom: 180.0,
                                       child: Icon(Feather.bookmark,
-                                      size: 25.0,
+                                      size: 26.0,
                                       color: Colors.white,)),
                                     Positioned(
                                       right: 20.0,
@@ -271,7 +311,9 @@ class _FeedPageState extends State<FeedPage> {
           ),
         ),
       ),
-    );
+      
+          backgroundColor: Colors.black,
+        );
   }
 }
 
--- lib/models/post.dart
+++ lib/models/post.dart
@@ -2,32 +2,46 @@ class Post {
   final String name;
   final String comment;
   final String imageUrl;
+  final String image2;
   final String likes;
   final String desc;
+  final String time;
+  final String user;
+  final bool hitLike;
 
-  Post({ this.name, this.comment, this.imageUrl, this.likes, this.desc});
+  Post({ this.name, this.comment, this.imageUrl,this.image2, this.likes, this.desc, this.time, this.user, this.hitLike});
 }
 
-final List <Post> posts = [
+final List <Post> post = [
   Post(
     name: 'John23',
     imageUrl: 'images/post0.jpg',
+    image2: 'images2/image20.jpg',
     comment: 'This is amazing! Awesome View!!!',
     likes: '1,012',
-    desc: 'hhfikdsajbfdkjsbaggreigrewuiajgewabuig\nfbeiwau'
+    desc: 'hhfikdsajbfdkjsbaggreigrewuiajgewabuig\nfbeiwau',
+    time: '45 mins ago',
+    user:'Jacob234',
   ),
   Post(
     name: 'JessicaSmith',
     comment: 'Really liking your collection!',
     imageUrl: 'images/post1.jpg',
+    image2: 'images2/image21.jpg',
     likes: '654',
-    desc: 'hhfikdsajbfdkjsbaggreagriehagre'
+    desc: 'hhfikdsajbfdkjsbaggreagriehagre',
+    time: '1 hour ago',
+    user: 'Bobby88',
     ),
   Post(
     name: 'Kelsey383',
     comment: 'Amazing photos! Keep it up!',
     imageUrl: 'images/post2.jpg',
+    image2: 'images2/image22.jpg',
     likes: '2,013',
-    desc: 'hhfikdsajbfdkjsbagGERKOWANIUGREA'
+    desc: 'hhfikdsajbfdkjsbagGERKOWANIUGREA',
+    time: '2 hours ago',
+    user: 'Clair100',
   ),
 ];
+
